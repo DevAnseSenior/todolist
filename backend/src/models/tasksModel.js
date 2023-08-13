7d61924a6793ea2742b76b3a4c8a811396f2cedd
Anderson Coelho <anderson.coelho1969@gmail.com>
2023-08-12 20:43:12 -0400

    def db connection and prepare the routes to use

---
+++ backend/src/models/tasksModel.js
@@ -0,0 +1,8 @@
+const getAll = () => {
+    
+};
+
+
+module.exports = {
+    getAll
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,3 +1,6 @@
 const app = require('./app');
+require('dotenv').config();
 
-app.listen(3333, () => console.log('Server running on port 3333'));
+const PORT = process.env.PORT || 3333;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
