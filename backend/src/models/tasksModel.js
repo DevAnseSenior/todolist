11bef58f967cba8fb23db0d4d8436d645405e545
Anderson Coelho <anderson.coelho1969@gmail.com>
2023-08-12 22:03:32 -0400

    Finish the list tasks route

--- backend/src/models/tasksModel.js
+++ backend/src/models/tasksModel.js
@@ -1,7 +1,7 @@
 const connection =  require('./connection');
 
 const getAll = async () => {
-    const tasks = await connection.execute('SELECT * FROM tasks');
+    const [tasks] = await connection.execute('SELECT * FROM tasks');
     return tasks;
 };
 
