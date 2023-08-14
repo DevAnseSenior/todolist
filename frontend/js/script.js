a130a3c8460b807683370717b044b386834b6781
Anderson Coelho <anderson.coelho1969@gmail.com>
2023-08-14 13:43:25 -0400

    mount the table structure by JS

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,3 +1,5 @@
+const tbody = document.querySelector('tbody');
+
 const fetchTasks = async () => {
     const response = await fetch('http://localhost:3333/tasks');
     const tasks = await response.json();
@@ -5,3 +7,69 @@ const fetchTasks = async () => {
     return tasks;
 }
 
+const createElement = (tag, innerText = '', innerHTML = '') =>  {
+    const element = document.createElement(tag);
+    
+    if (innerText) {
+        element.innerText = innerText;
+    }
+
+    if (innerHTML) {
+        element.innerHTML = innerHTML;
+    }
+
+    return element;
+}
+
+const createSelect = (value) => {
+    const options = `
+        <option value="pendente">Pendente</option>
+        <option value="em andamento">Em Andamento</option>
+        <option value="concluida">Concluída</option>
+    `;
+    const select = createElement('select', '', options);
+
+    select.value = value;
+
+    return select;
+}
+
+const task = {
+    "id": "0000",
+    "title": "teste",
+    "created_at": "00 de Janeiro de 2023 00:14",
+    "status": "concluida"
+}
+
+const createRow = (task) => {
+
+    const { id, title, created_at, status } = task;
+
+    const tr = createElement('tr');
+    const tdTitle = createElement('td', title);
+    const tdCreatedAt = createElement('td', created_at);
+    const tdStatus = createElement('td');
+    const tdActions = createElement('td');
+
+    const select = createSelect(status);
+
+    const editButton = createElement('button', '', '<span class="material-symbols-outlined">edit</span>');
+    const deleteButton = createElement('button', '', '<span class="material-symbols-outlined">delete</span>');
+
+    editButton.classList.add('btn-action');
+    deleteButton.classList.add('btn-action');
+
+    tdStatus.appendChild(select);
+
+    tdActions.appendChild(editButton);
+    tdActions.appendChild(deleteButton);
+
+    tr.appendChild(tdTitle);
+    tr.appendChild(tdCreatedAt);
+    tr.appendChild(tdStatus);
+    tr.appendChild(tdActions);
+
+    tbody.appendChild(tr);
+}
+
+createRow(task);
