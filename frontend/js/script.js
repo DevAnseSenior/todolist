ad71d750d01b095614940ab97edfaed5046044ae
Anderson Coelho <anderson.coelho1969@gmail.com>
2023-08-14 17:22:34 -0400

    function to update the status when user changes it

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -32,6 +32,17 @@ const deleteTask = async (id) => {
     loadTasks();
 }
 
+const updateTask = async ({ id, title, created_at, status }) => {
+
+    await fetch(`http://localhost:3333/tasks/${id}`, {
+        method: 'put',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ title, status })
+    });
+
+    loadTasks();
+}
+
 const formatDate = (dateUTC) => {
     const options = { dateStyle: 'long', timeStyle: 'short' };
     const date = new Date(dateUTC).toLocaleString('pt-BR', options);
@@ -77,6 +88,8 @@ const createRow = (task) => {
     const tdActions = createElement('td');
 
     const select = createSelect(status);
+
+    select.addEventListener('change', ({ target }) => updateTask({ ...task, status: target.value }));
 
     const editButton = createElement('button', '', '<span class="material-symbols-outlined">edit</span>');
     const deleteButton = createElement('button', '', '<span class="material-symbols-outlined">delete</span>');
